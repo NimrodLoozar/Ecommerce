ff115ecd491b95fb0d78f20495d65f735842f9fa
nimrod-code-cube <nimrod@code-cube.io>
2025-11-07 16:07:01 +0100

    Refactor code structure for improved readability and maintainability

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -15,13 +15,14 @@ class BrandSeeder extends Seeder
     {
         $brands = [
             // Brands with actual images in public/img/
-            ['name' => 'Renault', 'image' => 'img/renault.webp', 'sort_order' => 1],
-            ['name' => 'BMW', 'image' => 'img/BMW.webp', 'sort_order' => 2],
-            ['name' => 'Audi', 'image' => 'img/audi.avif', 'sort_order' => 3],
-            ['name' => 'Peugeot', 'image' => 'img/peugeot.jpg', 'sort_order' => 4],
-            ['name' => 'Mercedes-Benz', 'image' => 'img/mercedes.avif', 'sort_order' => 5],
-            ['name' => 'Lynk & Co', 'image' => 'img/link&co.jpg', 'sort_order' => 6],
-            ['name' => 'Citroën', 'image' => 'img/citroen.avif', 'sort_order' => 7],
+            ['name' => 'Renault', 'image' => 'img/Renault/renault.webp', 'sort_order' => 1],
+            ['name' => 'BMW', 'image' => 'img/BMW/BMW.webp', 'sort_order' => 2],
+            ['name' => 'Audi', 'image' => 'img/Audi/audi.avif', 'sort_order' => 3],
+            ['name' => 'Peugeot', 'image' => 'img/Peugeot/peugeot.jpg', 'sort_order' => 4],
+            ['name' => 'Mercedes-Benz', 'image' => 'img/Mercedes-Benz/mercedes.avif', 'sort_order' => 5],
+            ['name' => 'Lynk & Co', 'image' => 'img/Lynk&Co/link&co.jpg', 'sort_order' => 6],
+            ['name' => 'Citroën', 'image' => 'img/Citroën/citroen.avif', 'sort_order' => 7],
+            ['name' => 'BYD', 'image' => 'img/BYD/byd.jpg', 'sort_order' => 8],
 
             // Other brands without specific images (will use logo from svg folder or default)
             ['name' => 'Volkswagen', 'image' => null, 'sort_order' => 8],
